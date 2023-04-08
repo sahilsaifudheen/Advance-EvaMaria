85eeb47e9bc353f9f9cb25fdcfce78e7fbc89f2c
sahilsaifudheen <84787657+sahilsaifudheen@users.noreply.github.com>
2023-04-09 00:06:33 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,4 +1,3 @@
-
 from pyrogram import Client, filters
 import datetime
 import time
@@ -6,11 +5,13 @@ from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages
 import asyncio
-        
+
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
 async def verupikkals(bot, message):
-    users = await db.get_all_users()
+    users = None
+    async with db.get_all_users() as all_users:
+        users = [user async for user in all_users]
+
     b_msg = message.reply_to_message
     sts = await message.reply_text(
         text='Broadcasting your messages...'
@@ -20,11 +21,16 @@ async def verupikkals(bot, message):
     done = 0
     blocked = 0
     deleted = 0
-    failed =0
-
+    failed = 0
     success = 0
-    async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+
+    tasks = []
+    for user in users:
+        tasks.append(asyncio.ensure_future(broadcast_messages(int(user['id']), b_msg)))
+        await asyncio.sleep(0.05)
+
+    for task in asyncio.as_completed(tasks):
+        pti, sh = await task
         if pti:
             success += 1
         elif pti == False:
@@ -35,8 +41,8 @@ async def verupikkals(bot, message):
             elif sh == "Error":
                 failed += 1
         done += 1
-        await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
