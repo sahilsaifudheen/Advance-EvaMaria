be0d21d94191012f4fac0b8d51e7edc1b001c707
sahilsaifudheen <84787657+sahilsaifudheen@users.noreply.github.com>
2023-04-09 00:28:14 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,8 +2,17 @@
 from aiohttp import web
 from .route import routes
 
-
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
+async def main():
+    app = await web_server()
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, 'localhost', 8080)
+    await site.start()
+
+if __name__ == '__main__':
+    asyncio.run(main())
