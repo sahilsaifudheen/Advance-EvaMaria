d492b972211c8b4f8d9c0274bd5a9051dfca3f29
sahilsaifudheen <84787657+sahilsaifudheen@users.noreply.github.com>
2023-04-09 00:26:27 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,8 +1,12 @@
-#rymme
 from aiohttp import web
 
-routes = web.RouteTableDef()
-
-@routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("GreyMatter_Bots")
+
+app = web.Application()
+app.add_routes([
+    web.get("/", root_route_handler, allow_head=True),
+])
+
+if __name__ == "__main__":
+    web.run_app(app)
