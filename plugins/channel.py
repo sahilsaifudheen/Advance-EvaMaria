c20dbf5f2c54ebc3c22421584649c7bb04becbe9
sahilsaifudheen <84787657+sahilsaifudheen@users.noreply.github.com>
2023-04-09 00:08:30 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -6,7 +6,7 @@ media_filter = filters.document | filters.video | filters.audio
 
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
-async def media(bot, message):
+async def media_handler(client, message):
     """Media Handler"""
     for file_type in ("document", "video", "audio"):
         media = getattr(message, file_type, None)
@@ -17,4 +17,4 @@ async def media(bot, message):
 
     media.file_type = file_type
     media.caption = message.caption
-    await save_file(media)
+    await save_file(media, client)
